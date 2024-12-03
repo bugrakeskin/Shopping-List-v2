2f1ee714774ad3def1d93fd1ec3bce555e0c2883
Buğra Keskin <bugra.keskin@gmail.com>
2024-12-03 19:18:54 +0300

    forget navbar :(

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <NuxtLayout>
       <div class="dark:bg-gray-800 mx-auto max-w-xl min-h-screen justify-center flex flex-col">
-        <NavBar2 />
+        <NavBar />
         <NuxtPage class="flex-grow" />
         <Footer />
         <UNotifications />
